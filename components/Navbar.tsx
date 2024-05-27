235664754179b2740fbd09fc110d9f0034c35395
Swastik Dhar <swastikdhar18@gmail.com>
2024-05-27 20:09:22 +0530

    .

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import Link from 'next/link
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/" className=''>LOGO</Link>
+      <div>
+        <Button></Button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
